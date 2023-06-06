c1c29c2f46246f9357fe1b9d6d7592c4b8546190
Tuyết Trúc <ngothituyettruc2016@gmail.com>
2023-06-06 16:15:25 +0700

    process error state

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
+class App extends React.Component{
+  render () {
+    return (
     <div>
       Hello World with Bamboo &amp; Everyone
       <MyComponent></MyComponent>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello world!
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+
+
+
+    );
+  }
 }
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world!
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,12 +5,40 @@ import React from 'react';
 
 class MyComponent extends React.Component {
     
+    state = {
+        name: 'Bamboo',
+        address: 'Binh Thuan province',
+        age: 21
+    };
+
+    handleClick(event) {
+        console.log('>> click my by button')
+        // console.log(" Random ", Math.floor(Math.random() * 100) + 1)     
+        
+        
+        //merge State => react class
+        this.setState({
+            name: 'Tuyet Truc',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+        // this.setState({
+        //     age: Math.floor((Math.random() * 100) + 1)
+        // })
+    }
+
+    handleOnMouseOver(event){
+        // console.log(event)
+    }
+
     //JSX 
     render() {
         return(
 
-            <div>My first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and I'm from {this.state.age}
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <button onClick = {(event) => {this.handleClick(event)} }>Click me</button>
             </div>
             
         );
